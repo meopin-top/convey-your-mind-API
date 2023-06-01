3ad062354dab75665fd750332b3f4cfdef7248cb
JIH0LEE <skql5810@gmail.com>
2023-06-01 23:11:14 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/muffintop/cym/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package muffintop.cym.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/muffintop/cym/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package muffintop.cym.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

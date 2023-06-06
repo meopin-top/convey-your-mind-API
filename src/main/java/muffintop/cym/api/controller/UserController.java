1e1ea525a785f7bc9fb86f49f97f25464224ed65
JIH0LEE <skql5810@gmail.com>
2023-06-06 12:04:43 +0900

    feat: 패스워드 encoder 추가

--- src/main/java/muffintop/cym/api/controller/UserController.java
+++ src/main/java/muffintop/cym/api/controller/UserController.java
@@ -1,7 +1,9 @@
 package muffintop.cym.api.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import muffintop.cym.api.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/s")
+    public String test2(HttpServletRequest request){
+        System.out.println(request.getCookies().stream());
+        return "good";
+    }
+
 
 }

0404d00b24858bffbaf25858c7f9ea0abc0bd6ef
JIH0LEE <skql5810@gmail.com>
2023-07-09 14:03:57 +0900

    feat: 에러 메세지 변경

--- src/main/java/muffintop/cym/api/controller/enums/ResponseCode.java
+++ src/main/java/muffintop/cym/api/controller/enums/ResponseCode.java
@@ -9,16 +9,16 @@ public enum ResponseCode {
 
     //USER
     // 회원가입
-    SIGN_UP_SUCCESS(1001, "회원가입이 완료되었습니다"),
+    SIGN_UP_SUCCESS(1001, "회원가입이 완료되었습니다."),
     DUPLICATED_ID(1011, "이미 존재하는 ID 입니다."),
     INCORRECT_PASSWORD(1012, "패스워드가 일치하지 않습니다."),
-    INVALID_FORMAT_PASSWORD(1013, "패스워드의 형태가 유효하지 않습니다"),
-    INVALID_FORMAT_ID(1014, "ID의 형태가 유효하지 않습니다"),
-    INVALID_FORMAT_EMAIL(1015, "EMAIL의 형태가 유효하지 않습니다"),
+    INVALID_FORMAT_PASSWORD(1013, "패스워드의 형태가 유효하지 않습니다."),
+    INVALID_FORMAT_ID(1014, "ID의 형태가 유효하지 않습니다."),
+    INVALID_FORMAT_EMAIL(1015, "EMAIL의 형태가 유효하지 않습니다."),
 
     // 로그인
     SIGN_IN_SUCCESS(1101, "로그인이 완료되었습니다."),
-    NON_EXIST_ID(1111, "존재하지 않는 ID 입니다. 다시 한번 확인해 주세요"),
+    NON_EXIST_ID(1111, "존재하지 않는 ID 입니다. 다시 한번 확인해 주세요."),
     INVALID_PASSWORD(1112, "PW를 잘못 입력하셨습니다. 다시 한 번 확인해주세요."),
 
     // 파일

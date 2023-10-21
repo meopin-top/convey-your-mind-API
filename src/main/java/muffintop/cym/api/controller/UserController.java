5e205fc598e5e2179cd73f487f0c3adb6b9bb1c4
JIH0LEE <skql5810@gmail.com>
2023-10-22 01:23:18 +0900

    debugin

--- src/main/java/muffintop/cym/api/controller/UserController.java
+++ src/main/java/muffintop/cym/api/controller/UserController.java
@@ -44,7 +44,7 @@ public class UserController {
         Token token = tokenManager.generateNewToken(user);
         Cookie accessTokenCookie = new Cookie("AccessToken", token.getAccessToken());
         accessTokenCookie.setDomain("34.64.92.123");
-        accessTokenCookie.setHttpOnly(true);
+//        accessTokenCookie.setHttpOnly(true);
         accessTokenCookie.setPath("/");
         response.addCookie(accessTokenCookie);
         return ResponseHandler.generateResponse(ResponseCode.SIGN_UP_SUCCESS, HttpStatus.OK, user);
@@ -57,7 +57,7 @@ public class UserController {
         Token token = tokenManager.generateNewToken(user);
         Cookie accessTokenCookie = new Cookie("AccessToken", token.getAccessToken());
         accessTokenCookie.setDomain("34.64.92.123");
-        accessTokenCookie.setHttpOnly(true);
+//        accessTokenCookie.setHttpOnly(true);
         accessTokenCookie.setPath("/");
         response.addCookie(accessTokenCookie);
         System.out.println(accessTokenCookie);
@@ -74,7 +74,7 @@ public class UserController {
     public ResponseEntity<CommonResponse> logout(HttpServletResponse response) {
         Cookie accessTokenCookie = new Cookie("AccessToken", null);
         accessTokenCookie.setDomain("34.64.92.123");
-        accessTokenCookie.setHttpOnly(true);
+//        accessTokenCookie.setHttpOnly(true);
         accessTokenCookie.setPath("/");
         response.addCookie(accessTokenCookie);
         return ResponseHandler.generateResponse(ResponseCode.LOGOUT_SUCCESS, HttpStatus.OK, null);
